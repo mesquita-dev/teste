2736220dfe868f4f1c3734f1aeb3828dae2dc1c5
Lucas Mesquita <lmesquita2k@gmail.com>
2023-08-29 16:15:06 -0300

    teste

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,35 @@
 module.exports = {
   siteMetadata: {
     title: `nps`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
+    description: `Explore o Futuro da Experiência do Cliente: Sua Opinião é Fundamental! Participe da Nossa Pesquisa de Satisfação Exclusiva para Clientes e Ajude-nos a Aperfeiçoar Ainda Mais Nossos Serviços. Sua Voz Faz a Diferença!`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-use-query-params",
+    "gatsby-plugin-postcss", 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp", 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  {
+    resolve: `gatsby-plugin-web-font-loader`,
+    options: {
+      google: {
+        families: ['Mulish:300,400,500,600,700'],
+      },
+    },
+  },]
 };
---
+++ src/components/Search.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+import { useSearchParams } from 'react-router-dom';
+
+const Search = () => {
+  const [comment, setComment] = useState('')
+  const [searchParams, setSearchParams] = useSearchParams();
+  const scoreParam = searchParams.get('score');
+  const keyParam = searchParams.get('key');
+
+  const handlesubmit = (e) => {
+    console.log(e.target.value)
+  }
+
+  return (
+    <div className="max-w-[544px] flex flex-col py-6 p-3 h-full">
+      <p className="p-6 bg-gray-50 rounded-full border-2 border-gray-400 text-4xl font-medium text-gray-400 block mx-auto mb-4">
+        {scoreParam}
+      </p>
+      <h2 className="text-zinc-800 text-base sm:text-lg font-bold sm:font-semibold mb-2">
+        Olá {keyParam}, essa foi a sua nota na nossa pesquisa, gostaríamos de
+        saber se tem algum comentário ou observação a fazer.
+      </h2>
+
+      <form
+        action=""
+        onSubmit={(e) => handlesubmit(e)}
+        className="flex flex-col"
+      >
+        <input
+          type="search"
+          name="comment"
+          id="comment"
+          autoComplete="off"
+          placeholder="Digite aqui sua experiência"
+          className="pt-4 pb-32 pl-4 rounded-lg bg-netral-50 border border-stone-300 outline-0 mb-4"
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          className="py-4 px-6 bg-[#4510a3] text-white rounded-md max-w-fit mx-auto mb-4"
+          type="submit"
+        >
+          Enviar feedback
+        </button>
+        {scoreParam >= 9 ? (
+          <Link
+            className="py-4 px-6 bg-gray-200 text-black rounded-md max-w-fit mx-auto mb-6"
+            id=""
+            to="https://g.page/r/CdUtWWiBriboEAI/review"
+            target="_blank"
+          >
+            Quero enviar feedback para o Google também
+          </Link>
+        ) : null}
+        <p className="text-center">
+          Você também pode ver nossos conteúdos{' '}
+          <a
+            href="https://grantoseguros.com/blog"
+            target="_blank"
+            className="text-[#4510a3]"
+          >
+            clicando aqui nesse link
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
